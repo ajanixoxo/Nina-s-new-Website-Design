d90c4d2cc392327d062dc4f9bc8306ddab337353
ajanixoxo <joelayomide35@gmail.com>
2023-04-27 17:21:01 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -70,3 +70,45 @@ searchBtn2.onclick = function() {
 
   
 }
+var swipers = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints:{
+      0: {
+          slidesPerView: 1,
+      },
+      520: {
+          slidesPerView: 2,
+      },
+      950: {
+          slidesPerView: 3,
+      },
+  },
+});
+let fullImgBox = document.getElementById("fullImgBox");
+let fullImg = document.getElementById("fullImg");
+
+
+function openFullImg(pic){
+  fullImgBox.style.display = "flex"; 
+  fullImg.src = pic;
+}
+
+function closeFullImg(){
+  fullImgBox.style.display = "none"
+
+}
